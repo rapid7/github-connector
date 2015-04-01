6b12d774417e2b05c750ebbcc99ec6c122b5118e
Brandon Turner <bt@brandonturner.net>
2015-04-01 13:19:08 -0500

    Prepare v0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,13 @@
 GitHub Connector CHANGELOG
 ==========================
 
+v0.1.1
+------
+- Update to Rails 4.2.1
+- Update cookbook to Ruby 2.2.1
+- Add a call to FileUtils.mkdir_p before running slapd [#2]
+- Check ConnectionStatus#error_message before rule failures [#4]
+
 v0.1.0
 ------
 - Initial open source release
--- cookbook/metadata.rb
+++ cookbook/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "engineeringservices@rapid7.com"
 license          "All rights reserved"
 description      "Installs and configures the GitHub Active Directory Connector"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.1.1"
 
 supports 'ubuntu'
 
